6b2ee2feb11222d0a594793083059f57cd6cba68
Omar A. Orga <orgaomar8@gmail.com>
2023-02-17 18:14:31 +0800

    Add buttons to game

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-function game() {
-  let computerScore = 0,
-    playerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    c = playRound();
-    if (c === 1) {
-      computerScore++;
-    } else if (c === 2) {
-      playerScore++;
-    }
-  }
-  if (playerScore > computerScore) {
-    return "You win!";
-  } else if (playerScore < computerScore) {
-    return "You lose!";
-  } else {
-    return "Its a Draw";
-  }
-}
+const rock = document.querySelector(`.btn1`);
+const paper = document.querySelector(`.btn2`);
+const scissor = document.querySelector(`.btn3`);
+const playerOptions = [rock,paper,scissor];
 
-function playRound() {
-  let playerSelection = window.prompt(`Input rock , paper , or scissor`);
+playerOptions.forEach( (playerOptions) => {
+  playerOptions.addEventListener(`click` , function(){
+    
+    function getComputerChoice() {
+      const pick = Math.floor(Math.random() * 3);
+      choices = ["rock", "paper", "scissor"];
+      return choices[pick];
+    }
+    computerSelection = getComputerChoice();
+    playerSelection = this.innerText;
+    
+    console.log("player:" + playerSelection);
+    console.log("computer:" + computerSelection);
+  
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+      console.log(`Its a Draw!`);
+      c = 3;
+      return c;
+    } else if (
+      (playerSelection.toLowerCase() === `rock` &&
+        computerSelection.toLowerCase() === `paper`) ||
+      (playerSelection.toLowerCase() === `paper` &&
+        computerSelection.toLowerCase() === `scissor`) ||
+      (playerSelection.toLowerCase() === `scissor` &&
+        computerSelection.toLowerCase() === `rock`)
+    ) {
+      console.log(
+        `You Lose!, ` +
+          computerSelection.toLowerCase() +
+          ` beats ` +
+          playerSelection.toLowerCase() +
+          `.`
+      );
+      c = 1;
+      return c;
+    } else {
+      console.log(
+        `You Win!, ` +
+          playerSelection.toLowerCase() +
+          ` beats ` +
+          computerSelection.toLowerCase() +
+          `.`
+      );
+      c = 2;
+      return c;
+    }
+  })
+})
+
+
+
+
+function playRound(playerSelection) {
 
   function getComputerChoice() {
     const pick = Math.floor(Math.random() * 3);
@@ -75,4 +111,3 @@ function playRound() {
     console.log(`Wrong input`);
   }
 }
-console.log(game());
