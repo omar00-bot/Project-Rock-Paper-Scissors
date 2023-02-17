8841c30e20af5451ed83eb1899326b6f960d70e5
Omar A. Orga <orgaomar8@gmail.com>
2023-02-17 18:29:26 +0800

    Update in script

--- script.js
+++ script.js
@@ -13,10 +13,12 @@ playerOptions.forEach( (playerOptions) => {
     }
     computerSelection = getComputerChoice();
     playerSelection = this.innerText;
-    
-    console.log("player:" + playerSelection);
-    console.log("computer:" + computerSelection);
-  
+
+    const computerParagraph = document.querySelector(`.paragraph1`);
+    const playerParagraph = document.querySelector(`.paragraph2`);
+    playerParagraph.textContent = `Player : ` + playerSelection;
+    computerParagraph.textContent =  `Computer : ` +computerSelection;
+
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
       console.log(`Its a Draw!`);
       c = 3;
@@ -51,63 +53,3 @@ playerOptions.forEach( (playerOptions) => {
     }
   })
 })
-
-
-
-
-function playRound(playerSelection) {
-
-  function getComputerChoice() {
-    const pick = Math.floor(Math.random() * 3);
-    if (pick === 3) {
-      getComputerChoice();
-    }
-    choices = ["rock", "paper", "scissor"];
-    return choices[pick];
-  }
-  computerSelection = getComputerChoice();
-  console.log("player:" + playerSelection);
-  console.log("computer:" + computerSelection);
-
-  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-    console.log(`Its a Draw!`);
-    c = 3;
-    return c;
-  } else if (
-    (playerSelection.toLowerCase() === `rock` &&
-      computerSelection.toLowerCase() === `paper`) ||
-    (playerSelection.toLowerCase() === `paper` &&
-      computerSelection.toLowerCase() === `scissor`) ||
-    (playerSelection.toLowerCase() === `scissor` &&
-      computerSelection.toLowerCase() === `rock`)
-  ) {
-    console.log(
-      `You Lose!, ` +
-        computerSelection.toLowerCase() +
-        ` beats ` +
-        playerSelection.toLowerCase() +
-        `.`
-    );
-    c = 1;
-    return c;
-  } else if (
-    (playerSelection.toLowerCase() === `paper` &&
-      computerSelection.toLowerCase() === `rock`) ||
-    (playerSelection.toLowerCase() === `scissor` &&
-      computerSelection.toLowerCase() === `paper`) ||
-    (playerSelection.toLowerCase() === `rock` &&
-      computerSelection.toLowerCase() === `scissor`)
-  ) {
-    console.log(
-      `You Win!, ` +
-        playerSelection.toLowerCase() +
-        ` beats ` +
-        computerSelection.toLowerCase() +
-        `.`
-    );
-    c = 2;
-    return c;
-  } else {
-    console.log(`Wrong input`);
-  }
-}
