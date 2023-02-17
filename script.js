04b229a19dc9624c87625c45bf85b49f7d18d5b7
Omar A. Orga <orgaomar8@gmail.com>
2023-02-17 22:27:26 +0800

    Make the html and add objects

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let playerScore = 0;
+let computerScore = 0;
+let movesLeft = 0
+const computerParagraph = document.querySelector(`.paragraph1`);
+const playerParagraph = document.querySelector(`.paragraph2`);
+const resultParagraph = document.querySelector(`.paragraph3`);
+const gameoverParagraph = document.querySelector(`.paragraph4`);
 const rock = document.querySelector(`.btn1`);
 const paper = document.querySelector(`.btn2`);
 const scissor = document.querySelector(`.btn3`);
@@ -14,42 +21,42 @@ playerOptions.forEach( (playerOptions) => {
     computerSelection = getComputerChoice();
     playerSelection = this.innerText;
 
-    const computerParagraph = document.querySelector(`.paragraph1`);
-    const playerParagraph = document.querySelector(`.paragraph2`);
     playerParagraph.textContent = `Player : ` + playerSelection;
     computerParagraph.textContent =  `Computer : ` +computerSelection;
 
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-      console.log(`Its a Draw!`);
-      c = 3;
-      return c;
-    } else if (
-      (playerSelection.toLowerCase() === `rock` &&
-        computerSelection.toLowerCase() === `paper`) ||
-      (playerSelection.toLowerCase() === `paper` &&
-        computerSelection.toLowerCase() === `scissor`) ||
-      (playerSelection.toLowerCase() === `scissor` &&
-        computerSelection.toLowerCase() === `rock`)
-    ) {
-      console.log(
-        `You Lose!, ` +
-          computerSelection.toLowerCase() +
-          ` beats ` +
-          playerSelection.toLowerCase() +
-          `.`
-      );
-      c = 1;
-      return c;
-    } else {
-      console.log(
-        `You Win!, ` +
-          playerSelection.toLowerCase() +
-          ` beats ` +
-          computerSelection.toLowerCase() +
-          `.`
-      );
-      c = 2;
-      return c;
+    playRound(playerSelection , computerSelection);
+    movesLeft++;
+
+    if(movesLeft===10){
+      gameOver(playerScore,computerScore);
     }
   })
 })
+
+function playRound(playerSelection , computerSelection){
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    resultParagraph.textContent = `Its a Draw!`
+  } else if (
+    (playerSelection.toLowerCase() === `rock` &&
+      computerSelection.toLowerCase() === `paper`) ||
+    (playerSelection.toLowerCase() === `paper` &&
+      computerSelection.toLowerCase() === `scissor`) ||
+    (playerSelection.toLowerCase() === `scissor` &&
+      computerSelection.toLowerCase() === `rock`)
+  ) {
+    resultParagraph.textContent = `You Lose!, ` + computerSelection.toLowerCase() +` beats ` +playerSelection.toLowerCase() +`.`;
+    computerScore++;
+  } else {
+    resultParagraph.textContent = `You Lose!, ` + computerSelection.toLowerCase() +` beats ` +playerSelection.toLowerCase() +`.`;
+    playerScore++;
+  }
+}
+
+function gameOver(){
+  if(playerScore>computerScore){
+    gameoverParagraph.textContent = `Game Over! You Win!`;
+  }
+  else {
+    gameoverParagraph.textContent = `Game Over! You Lose!`;
+  }
+}
